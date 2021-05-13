84959be14c51d3673a0e392aadc5760fa8546cdb
pedromarcal12 <henrique.pedro.marcal@gmail.com>
2021-05-13 22:47:29 +0100

    Random user created

--- code.js
+++ code.js
@@ -59,32 +59,4 @@ var faker = require('faker'); // Faker.js library
 api.username = faker.internet.userName();
 api.password = faker.internet.password();
 
-$.ajax({
-  url: 'https://randomuser.me/api/',
-  dataType: 'json',
-  success: function(data) {
-    console.log(data);
-  }
-});
 
- var total_items = 4;
-
-        function CalculateItemsValue(){
-            var total = 0;
-
-            for (let i = 1; i <= total_items; i++) {
-               itemID =document.getElementById("qnt_" + i);
-               total = total + parseInt(itemID.value) * parseInt(itemID.getAttribute("data-price"));
-                
-            }
-            document.getElementById('ItemsTotal').innerHTML = "€" + total;
-        }
-
-        document.querySelectorAll('[id^="qnt_"]').forEach(item => {
-            item.addEventListener('keyup', CalculateItemsValue);
-        });
-
-        function getUser () {
-          fetch
-
-        }
